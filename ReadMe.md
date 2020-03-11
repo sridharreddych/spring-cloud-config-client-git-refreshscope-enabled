5dd4ddd4cacc31b5ca666805192f30144ebddebd
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-03-11 14:46:40 +0530

    first commit, config client to consume values from config server git

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+Config client to read the values from config server (git based)
+http://localhost:8080/rate"# sree-springcloud-m2-config-client" 
---
+++ src/main/java/sree/demo/SreeSpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package sree.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreeSpringcloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreeSpringcloudM2ConfigClientApplication.class, args);
+	}
+
+}
